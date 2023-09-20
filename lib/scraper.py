3f0910d5813447d70d606a0598fe5d0e0cf69479
Clae <56447997+clayruh@users.noreply.github.com>
2023-09-20 17:11:19 -0400

    working

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,21 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+
+# returns a list of bs4.element.NavigableString objects
+print(doc.select('.heading-primary')[0].contents)
+
+# returns the text without extra space
+print(doc.select('.heading-primary')[0].text.strip())
+
+# returns a list of bs4.element.ResultSet, a lot like a list
+# each one is a bs4.element.Tag object
+courses = doc.select('.heading-25-extrabold.color-black')
+for course in courses:
+    # prints all the headings as text!
+    print(course.contents[0].strip())
+# print the name attribute, which is the HTML tag
+# in this case, h3 tag
+print(courses[0].name)
